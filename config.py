694003e7d374db9c3113cc22ce38a0e16ef1b455
𝚨𝛂ѕͱ꧊𝐢͢к <teamaashik@gmail.com>
2025-08-11 17:44:23 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,21 @@
+API_ID = "4014305"
+
+API_HASH = "b0cb9e17b2b8bcde3be7161c8bfa6013"
+
+# Telegram bot token
+TELEGRAM_TOKEN = "7917878224:AAE9OJDudn21vny_sbEQhM8WpuZ4Vqwc4bU"
+
+# Your telegram user ID (owner)
+OWNER_ID = 8056154987
+
+SUDO_ID = [7163382841]
+
+MONGO_URI = "mongodb+srv://shivang:shiv@cluster0.2bupd.mongodb.net/?retryWrites=true&w=majority"
+
+DB_NAME = "x"
+
+LOGGER = True
+
+BOT_NAME = "Edit Guardian"
+
+SUPPORT_ID = (-1002354552656)
