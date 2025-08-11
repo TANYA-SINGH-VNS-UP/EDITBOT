9d7a43e9e73bcfae3fddd69fb554802e066887f3
𝚨𝛂ѕͱ꧊𝐢͢к <teamaashik@gmail.com>
2025-08-11 17:52:14 +0530

    𝐀𝐚𝐬𝐡𝐢𝐤 𝐓ᴇᴀᴍ

--- Aashik-Edit/main.py
+++ Aashik-Edit/main.py
@@ -4,7 +4,7 @@ import re
 import asyncio
 import time
 from random import choice
-from Aashik-Edit import *
+from Edit import *
 from telegram import Update, Bot
 from pyrogram import Client, filters
 from pyrogram.types import Message
