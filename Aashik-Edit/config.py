28c0b74488372bdded35d19bf984a98f275e217d
TANYA SINGH <tanyasingh2feb2009@gmail.com>
2025-09-07 16:44:37 +0530

    Update config.py

--- Aashik-Edit/config.py
+++ Aashik-Edit/config.py
@@ -8,7 +8,7 @@ API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 OWNER_ID = int(os.getenv("OWNER_ID"))
-SUDO_ID = int(os.getenv("SUDO_ID"))
+SUDO_ID = int(os.getenv("5288388597,6972508083"))
 MONGO_URI = os.getenv("MONGO_URI")
 DB_NAME = os.getenv("DB_NAME")
 LOGGER = os.getenv("LOGGER", "True").lower() == "true"
