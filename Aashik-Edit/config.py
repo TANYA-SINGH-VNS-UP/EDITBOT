185912f69b26859a8763fb30ea1bc6c596286828
TANYA SINGH <tanyasingh2feb2009@gmail.com>
2025-09-07 16:47:54 +0530

    Update config.py

--- Aashik-Edit/config.py
+++ Aashik-Edit/config.py
@@ -1,14 +1,17 @@
 import os
 from dotenv import load_dotenv
 
-# Load environment variables from .env file
 load_dotenv()
 
 API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 OWNER_ID = int(os.getenv("OWNER_ID"))
-SUDO_ID = int(os.getenv("5288388597,6972508083"))
+
+# SUDO_ID को list बनाओ
+SUDO_ID = os.getenv("SUDO_ID", "")
+SUDO_ID = [int(x) for x in SUDO_ID.split(",") if x.strip().isdigit()]
+
 MONGO_URI = os.getenv("MONGO_URI")
 DB_NAME = os.getenv("DB_NAME")
 LOGGER = os.getenv("LOGGER", "True").lower() == "true"
